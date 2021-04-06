494bd3125a0d12bce8d46ff284caf9f57afedf95
GastonKhouri <gastonlozano2016@yahoo.com>
2021-04-05 22:25:03 -0430

    Aplicación lista

---
+++ docs/precache-manifest.0f23bcb1a8022303e45fc981bcc3fbe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c07b115836ce75174fe75821758736",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09e8385adad96e911656",
+    "url": "/static/css/main.bcefd485.chunk.css"
+  },
+  {
+    "revision": "398a8e9a3bb0c35c487d",
+    "url": "/static/js/2.98c055bd.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.98c055bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e8385adad96e911656",
+    "url": "/static/js/main.8579cadd.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
